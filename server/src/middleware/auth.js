e4de2d2f079a015214964a7beeb54aa8e4f9faaf
Manish Jha <58687464+manjha28@users.noreply.github.com>
2021-10-18 01:32:25 +0800

    Update : Created Authorization middleware and validator

---
+++ server/src/middleware/auth.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+module.exports = async (req, res, next) => {
+  try {
+    //1. destructure the token from fetch request
+    const jwtToken = req.header("token");
+
+    if (!jwtToken) {
+      return res.status(403).json("User Not Authorized");
+    }
+    const payload = jwt.verify(jwtToken, process.env.PRIVATE_KEY);
+
+    req.user = payload.user;
+  } catch (error) {
+    console.error(error.message);
+    return res.status(403).json("User Not Authorized");
+  }
+};
--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,20 +1,21 @@
-const router = require('express').Router();
-const bcrypt = require('bcryptjs/dist/bcrypt');
-const pool = require("../db/index")
-const jwtGenerator = require("../utils/jwtGenerator")
-
+const router = require("express").Router();
+const bcrypt = require("bcryptjs/dist/bcrypt");
+const pool = require("../db/index");
+const jwtGenerator = require("../utils/jwtGenerator");
+const validInfo = require("../middleware/validInfo");
 
 //Register
-router.post("/register", async(req, res)=>{
-  try{
-
+router.post("/register",validInfo, async (req, res) => {
+  try {
     //1. destructure the req.body (name,email,password)
-    const {name, email, password} = req.body;
+    const { name, email, password } = req.body;
     //2. check if user exist
-    const user = await pool.query("SELECT * FROM userstb WHERE email =$1", [email]);
-    if (user.rows.length !==0){
-      return res.status(401).send("User already exists");  
-    };
+    const user = await pool.query("SELECT * FROM userstb WHERE email =$1", [
+      email,
+    ]);
+    if (user.rows.length !== 0) {
+      return res.status(401).send("User already exists");
+    }
     //3.bcrypt user password
 
     const saltRound = 10;
@@ -23,57 +24,53 @@ router.post("/register", async(req, res)=>{
 
     //4.add the new user insode the databse
 
-    const newUser = await pool.query("INSERT INTO userstb(name,email,password) VALUES ($1,$2,$3) RETURNING *",
-    [name,email,bcryptPassword]);
+    const newUser = await pool.query(
+      "INSERT INTO userstb(name,email,password) VALUES ($1,$2,$3) RETURNING *",
+      [name, email, bcryptPassword]
+    );
 
     // res.json(newUser.rows[0])
     //5.genrater jettoken
 
     const token = jwtGenerator(newUser.rows[0].login_id);
-    res.json({token});
-
-  }catch(err){
+    res.json({ token });
+  } catch (err) {
     console.error(err.message);
     res.status(500).send(`${err.message}`);
-
   }
-} )
-
+});
 
 //login Route
 
-router.post("/login", async(req, res)=>{
-
+router.post("/login",validInfo,async (req, res) => {
   try {
-    
     //1. destructure req.body
-    const{email, password} = req.body;
-    
-    
-    //2. check if user exists(if not then register)
-    const user = await pool.query("SELECT * FROM userstb WHERE email = $1", [email]);
+    const { email, password } = req.body;
 
-    if (user.rows.length === 0){
-      return res.status(401).send(`$email does not exist`)
+    //2. check if user exists(if not then register)
+    const user = await pool.query("SELECT * FROM userstb WHERE email = $1", [
+      email,
+    ]);
+
+    if (user.rows.length === 0) {
+      return res.status(401).send(`$email does not exist`);
     }
 
     //3. check if incoming password is same as stored password
 
-    const validPassword = await bcrypt.compare(password, user.rows[0].password)
+    const validPassword = await bcrypt.compare(password, user.rows[0].password);
     // console.log(validPassword)
-    if(!validPassword){
-      return res.status(401).send("invalid password")
+    if (!validPassword) {
+      return res.status(401).send("invalid password");
     }
 
     //4.//give jwt token
 
     const token = jwtGenerator(user.rows[0].login_id);
-    res.json({token}); 
-    
+    res.json({ token });
   } catch (err) {
     console.error(err.message);
-    res.status(500).send(`${error.message}`)
-    
+    res.status(500).send(`${error.message}`);
   }
-})
-module.exports =router;
+});
+module.exports = router;
