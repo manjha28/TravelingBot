582e7b7df23ca42cd47a585fcceeec978037f846
Manish Jha <58687464+manjha28@users.noreply.github.com>
2021-10-19 12:45:35 +0530

    Update : Register form

--- client/src/components/register.js
+++ client/src/components/register.js
@@ -1,12 +1,32 @@
 import React, { Fragment } from "react";
 
 const Register = (setAuth) => {
-    return (
-        <Fragment>
-            <h1>Register</h1>
-            <button onClick={() => setAuth(true)}>Register</button>
-        </Fragment>
-    );
+  return (
+    <Fragment>
+      <h1>Register</h1>
+      <form>
+        <input
+          className="form-control my-3"
+          type={"text"}
+          name="Name"
+          placeholder="Full Name"
+        />
+        <input
+          className="form-control my-3"
+          type={"email"}
+          name="email"
+          placeholder="Valid email"
+        />
+        <input
+          className="form-control my-3"
+          type={"email"}
+          name="Passqoed"
+          placeholder="Password"
+        />
+      </form>
+      <button onClick={() => setAuth(true)}>Register</button>
+    </Fragment>
+  );
 };
 
 export default Register;
